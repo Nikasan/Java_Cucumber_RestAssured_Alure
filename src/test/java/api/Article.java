566d1d8a8f4f50ed80e26eca66d5049075bc7db1
Veranika_Pekhtserava <@>
2021-04-12 09:51:23 +0200

    1 part of video

---
+++ src/test/java/api/Article.java
@@ -0,0 +1,25 @@
+
+package api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Article {
+    private List<Object> categories;
+    private String content;
+    private String date;
+    private String lang;
+    private String layout;
+    private List<Object> tags;
+    private String title;
+    private String url;
+
+}
